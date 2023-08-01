f00510c10bef5ea0b2a5ccf0c6cedc7a7082f80b
juanosorioz <elosoriojuan@gmail.com>
2023-08-01 09:58:09 -0500

    Terminación de proteger las rutas del perfil

--- servidor/src/middlewares/tokenValidation.js
+++ servidor/src/middlewares/tokenValidation.js
@@ -4,7 +4,13 @@ import { TOKEN_SECRET } from '../config.js'
 export const requireAuth = (req, res, next) => {
   //console.log(req.headers)
   const { token } = req.cookies;
-  console.log(cookies)
+  //console.log(token)
   if(!token)
   return res.status(401).json({ message: "No token, Authorization Denied" })
+
+  jwt.verify(token, TOKEN_SECRET, (err, user)=>{
+    if(err) res.status(403).json({message: "Invalid Token"})
+    req.user = user;
+    next();
+  })
 }
