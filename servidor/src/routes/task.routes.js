ecd856eb16a9b02a769bb8a3cee2a89723a034f7
juanosorioz <elosoriojuan@gmail.com>
2023-08-04 08:18:03 -0500

    Terminación de validadción, Creación de frontend, instalar dependencias, crear primer formulario y crear archivos

--- servidor/src/routes/task.routes.js
+++ servidor/src/routes/task.routes.js
@@ -1,12 +1,14 @@
 import { Router } from "express"
 import { getTask, getTasks, createTask, deleteTask, updateTask } from "../controllers/task.controllers.js"
 import { requireAuth } from "../middlewares/tokenValidation.js"
+import { createTaskSchema } from '../schema/task.schema.js'
+import { validateSchema } from "../middlewares/validator.middleware.js"
 
 const router = Router()
 
 router.get('/tasks', requireAuth,getTasks)
 router.get('/task/:id', requireAuth,getTask)
-router.post('/task', requireAuth,createTask)
+router.post('/task', requireAuth,validateSchema(createTaskSchema),createTask)
 router.put('/task/:id', requireAuth,updateTask)
 router.delete('/task/:id', requireAuth,deleteTask)
 
